1188486c7ee565fc074fdf481c6b071ba4d5b00a
Victoria Mota de Almeida <s2it_valmeida@uolinc.com>
2021-05-07 10:24:01 -0300

    Primeiro commit

---
+++ src/test/java/com/victoria/agendalive/AgendaliveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victoria.agendalive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaliveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
